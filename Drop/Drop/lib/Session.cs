f369945b4deaca9d7545154312d55e44c19a127e
Kireck211 <erickdesantiago11@gmail.com>
2016-04-25 13:50:33 -0500

    Skeleton of Drop

---
+++ Drop/Drop/lib/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drop
+{
+    /// <summary>
+    /// Creates a new Session.
+    /// </summary>
+    public class Session
+    {
+    }
+}
