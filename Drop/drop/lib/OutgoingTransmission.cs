aa735038c133479e9a99cea5e95b099f54f23d0a
Kireck211 <erickdesantiago11@gmail.com>
2016-04-25 15:11:30 -0500

    Refreshing the skeleton from some classes

--- Drop/drop/lib/OutgoingTransmission.cs
+++ Drop/drop/lib/OutgoingTransmission.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace Drop.lib
 {
+    /// <summary>
+    /// This class represents an outgoing transmission used to send files to another computer.
+    /// </summary>
     class OutgoingTransmission
     {
+        /// <summary>
+        /// The dbus-path of the current transmission.
+        /// </summary>
+        public string interface_path;
+
+        /// <summary>
+        /// Creates a new interface for the dbus-interface of an outgoing transmission.
+        /// </summary>
+        /// <param name="interface_path"></param>
+        public OutgoingTransmission (string interface_path)
+        {
+            return ;
+        }
+
+        /// <summary>
+        /// Lists the requested files for the transmission.
+        /// </summary>
+        /// <returns></returns>
+        public List<OutgoingFileRequest> get_file_requests () 
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Cancels the transmission.
+        /// </summary>
+        public void cancel()
+        {
+
+        }
+
+        /// <summary>
+        /// Returns if the current connection is encrypted.
+        /// </summary>
+        /// <returns></returns>
+        public bool get_is_secure()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Returns the current protocol state.
+        /// </summary>
+        /// <returns></returns>
+        public void /*ClientState*/ get_state()
+        {
+
+        }
+
+        /// <summary>
+        /// Returns the used protocol version of the connected server.
+        /// </summary>
+        /// <returns></returns>
+        public UInt16 get_server_version ()
+        {
+            return 1;
+        }
+
+        /// <summary>
+        /// Returns the display name of the connected server.
+        /// </summary>
+        /// <returns></returns>
+        public string get_server_name()
+        {
+            return "";
+        }
+
+        // Signals 
     }
 }
--- Drop/drop/lib/Settings.cs
+++ Drop/drop/lib/Settings.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Drop.lib
 {
+    /// <summary>
+    /// This class provides access to the configuration of the drop daemon.
+    /// </summary>
     class Settings
     {
+        /// <summary>
+        /// The name of the server that's displayed to the user. Leave this empty to use the real user name.
+        /// </summary>
+        public string server_name;
+
+        /// <summary>
+        /// Sets wether the server is enabled and should receive transmissions.
+        /// </summary>
+        public bool server_enabled;
+
+        /// <summary>
+        /// Creates a new Settings interface.
+        /// </summary>
+        public Settings()
+        {
+            
+        }
+
+        /// <summary>
+        /// Returns the same value like server_name, but already checks, if it's empty and returns the real user name instead.
+        /// </summary>
+        /// <returns>server_name</returns>
+        public string get_display_name()
+        {
+            return "";
+        }
     }
 }
