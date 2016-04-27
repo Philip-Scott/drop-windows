ba1ed3455a3b9e3edabce1eb98a30a2ad84a6b5a
Kireck211 <erickdesantiago11@gmail.com>
2016-04-26 23:10:14 -0500

    Skeleton for back end

---
+++ Drop/Session.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drop
+{
+    /// <summary>
+    /// Creates a new Session.
+    /// </summary>
+    public class Session
+    {
+    }
+
+}
---
+++ Drop/TransmissionPartner.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drop
+{
+    /// <summary>
+    /// Represents a transmission partner you can connect to.
+    /// </summary>
+    class TransmissionPartner
+    {
+        /// <summary>
+        /// The name of the transmission partner, acting as an ID.
+        /// </summary>
+        public string name;
+
+        /// <summary>
+        /// The hostname of the computer.
+        /// </summary>
+        public string hostname;
+
+        /// <summary>
+        /// The port of the service. This port requires a TLS handshake.
+        /// </summary>
+        public UInt16 port;
+
+        /// <summary>
+        /// The unencrypted port of the service. This port doesn't allow TLS, make sure that the user does not send critical files using this port.
+        /// </summary>
+        public UInt16 unencrypeted_port;
+
+        /// <summary>
+        /// The version of the protocol the server uses.
+        /// </summary>
+        public int protocol_implementation;
+
+        /// <summary>
+        /// The name of the server's protocol implementation.
+        /// </summary>
+        public string procotol_implementation;
+
+        /// <summary>
+        /// The server-name that should be displayed to the user.
+        /// </summary>
+        public string display_name;
+
+        /// <summary>
+        /// If true, the server is running and accepts transmissions.
+        /// </summary>
+        public bool server_enabled;
+    }
+
+}
