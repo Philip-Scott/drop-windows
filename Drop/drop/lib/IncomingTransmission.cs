d2beb2c854b1a7461232b75be175f736265469f4
Santiago Jacobus <wings_overripe_3@hotmail.com>
2016-05-13 03:26:37 -0500

    Methods with the structure of C#, missing implementation of said methods.

--- Drop/drop/lib/IncomingTransmission.cs
+++ Drop/drop/lib/IncomingTransmission.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,8 @@ namespace Drop.lib
 {
     class IncomingTransmission
     {
-        public enum ServerState {
+        public enum ServerState
+        {
             AWAITING_INITIALISATION,
             SENDING_INITIALISATION,
             AWAITING_REQUEST,
@@ -29,19 +32,90 @@ namespace Drop.lib
             bool accepted;
         }
 
-        public void protocol_failed(string error_message);
-        public void state_changed(ServerState state);
-        public void progress_changed(System.UInt64 bytes_received, System.UInt64 total_size);
-        public void file_received(uint id, string filename);
+        //public event protocol_failed(string error_message);
+        //public ev state_changed(ServerState state);
+        //public void progress_changed(System.UInt64 bytes_received, System.UInt64 total_size);
+        //public void file_received(uint id, string filename);
 
         private bool is_secure = false;
         private ServerState state = ServerState.AWAITING_INITIALISATION;
 
         private System.SByte client_version;
         private string client_name;
-        private Gee.HashMap< System.UInt16, FileRequest?> file_requests;
+        private ArrayList file_requests = new ArrayList();
+        public IncomingTransmission(Stream connection, string server_name, bool is_secure)
+        {
+           
+        }
 
+        public void reject_transmission()
+        {
+            
+        }
 
+        public void accept_transmission(System.UInt16[] ids)
+        {
+            
+        }
 
+        public void cancel()
+        {
+        }
+
+        public bool get_is_secure()
+        {
+            return is_secure;
+        }
+
+        public ServerState get_state()
+        {
+            return state;
+        }
+
+        public System.SByte get_client_version()
+        {
+            return client_version;
+        }
+
+        public string get_client_name()
+        {
+            return client_name;
+        }
+
+        public FileRequest[] get_file_requests()
+        {
+            FileRequest[] fr;
+            return fr;
+        }
+
+        private bool receive_initialisation()
+        {
+            return false;
+        }
+
+        private bool send_initialisation(string server_name)
+        {
+            return false;
+        }
+
+        private bool receive_request()
+        {
+            return false;
+        }
+
+        private void remember_accepted_ids(System.UInt16[] accepted_ids)
+        {
+            
+        }
+
+        private bool receive_data()
+        {
+            return false;
+        }
+
+        private void update_state(ServerState state)
+        {
+
+        }
     }
 }
