3d459b2aae471e193e6a98d0b5e74f625dba8248
Santiago Jacobus <wings_overripe_3@hotmail.com>
2016-05-12 23:54:10 -0500

    Translating Vala-Server, which is in the Vala language, to C#

--- Drop/drop/lib/IncomingTransmission.cs
+++ Drop/drop/lib/IncomingTransmission.cs
@@ -8,5 +8,25 @@ namespace Drop.lib
 {
     class IncomingTransmission
     {
+        public enum ServerState {
+            AWAITING_INITIALISATION,
+            SENDING_INITIALISATION,
+            AWAITING_REQUEST,
+            NEEDS_CONFIRMATION,
+            SENDING_CONFIRMATION,
+            REJECTED,
+            RECEIVING_DATA,
+            FINISHED,
+            CANCELED,
+            FAILURE
+        }
+
+        public struct FileRequest
+        {
+            //uint16 id;
+            //uint64 size;
+            string name;
+            bool accepted;
+        }
     }
 }
